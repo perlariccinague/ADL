448ceddb04fdc076b6b3abeabb675a8e0afec389
PN <pn@klapproth-koch.de>
2023-09-07 08:29:04 +0200

    Initial commit

---
+++ templates/footer.php
@@ -0,0 +1,12 @@
+<div class="footer">
+    {{insert_content::3}}
+    <div class="footer-middle">
+        {{insert_module::4}}
+        <div class="footer-right">
+            {{insert_content::33}}
+            {{insert_content::34}}
+            {{insert_content::35}}
+            {{insert_module::5}}
+        </div>
+    </div>
+</div>
---
+++ templates/header.php
@@ -0,0 +1,9 @@
+<div class="header">
+    <div class="header-left">
+        {{insert_content::3}}
+    </div>
+    <div class="header-right">
+        {{insert_module::1}}
+    </div>
+</div>
+
