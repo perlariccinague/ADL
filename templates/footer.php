b67dbd553b18765d55f241ec41618a2fb36cdb67
PN <pn@klapproth-koch.de>
2023-09-08 07:38:53 +0200

    Home page

--- templates/footer.php
+++ templates/footer.php
@@ -1,12 +1,22 @@
-<div class="footer">
-    {{insert_content::3}}
-    <div class="footer-middle">
-        {{insert_module::4}}
-        <div class="footer-right">
-            {{insert_content::33}}
-            {{insert_content::34}}
-            {{insert_content::35}}
-            {{insert_module::5}}
+<div class="bg-footer">
+    <div class="footer-top">
+        <div class="footer-logo">
+            {{insert_content::3}}
         </div>
+        <div class="footer-middle">
+            {{insert_module::4}}
+            <div class="footer-right">
+                {{insert_content::33}}
+                {{insert_content::34}}
+                {{insert_content::35}}
+                {{insert_module::5}}
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="footer-bottom">
+    <div class="nav-bottom">
+        {{insert_module::7}}
     </div>
 </div>
