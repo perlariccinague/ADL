cbc20bf91544f42f9fbff757e331fac8273ef087
SamesOfAm <flo.tepelmann@ewetel.net>
2023-11-14 17:17:32 +0100

    Mobile Korrekturen

--- templates/menu_mobile.php
+++ templates/menu_mobile.php
@@ -15,23 +15,18 @@
 <nav class="mod_navigation-mobile">
     <ul class="menu-list">
         <li>
-            <div>Vermögensverwaltung und Anlageberatung</div>
             {{insert_module::21}}
         </li>
         <li>
-            <div>Immobilien und Finanzierung</div>
             {{insert_module::22}}
         </li>
         <li>
-            <div>Unternehmens- und Wirtschaftsberatung</div>
             {{insert_module::23}}
         </li>
         <li>
-            <div>Über Adlatus</div>
             {{insert_module::24}}
         </li>
         <li class="last">
-            <div>Karriere</div>
             {{insert_module::25}}
         </li>
     </ul>
